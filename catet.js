4457429229a9831ab6d22d7bd1419d0390c4a131
Reza <hungry.dev39@gmail.com>
2021-03-13 00:15:13 +0700

    make add list functionality

--- catet.js
+++ catet.js
@@ -19,8 +19,23 @@ const screen = blessed.screen({
 
 screen.title = 'Catet | Simple TUI Notes Taking App';
 
+const notesForm = blessed.form({
+  left: 1,
+  width: '50%',
+  height: 3,
+  parent: screen,
+  border: {
+    type: 'line',
+  },
+});
+
+const notesInput = blessed.textbox({
+  parent: notesForm,
+  inputOnFocus: true,
+})
+
 const notesListBox = blessed.list({
-  top: 1,
+  top: 3,
   left: 1,
   width: '50%',
   height: '90%',
@@ -28,7 +43,6 @@ const notesListBox = blessed.list({
   border: {
     type: 'line'
   },
-  padding: 1,
   style: {
     border: {
       fg: '#fff'
@@ -45,13 +59,27 @@ const notesListBox = blessed.list({
   vi: true,
   search: true,
   items: fakeNotes.map(n => n.title),
+  parent: screen,
 });
-
-screen.append(notesListBox);
 
 // Quit on Escape, q, or Control-C.
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
   return process.exit(0);
+});
+
+screen.key('a', () => {
+  notesInput.clearValue();
+  screen.render();
+  notesInput.focus()
+});
+
+notesInput.on('submit', function() {
+  fakeNotes.push({
+    title: this.value,
+    content: '',
+  })
+  notesListBox.addItem(this.value);
+  screen.render();
 });
 
 screen.render();
