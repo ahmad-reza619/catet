87a1dd874dcbe188ea45ca06f234cf2498395af8
Reza <hungry.dev39@gmail.com>
2021-03-14 18:09:13 +0700

    create content form & input

--- src/input.js
+++ src/input.js
@@ -2,6 +2,7 @@ const blessed = require('blessed');
 const screen = require('./screen');
 const list = require('./list');
 const db = require('./db');
+const selectedStore = require('./store');
 
 const addForm = blessed.form({
   left: 1,
@@ -37,7 +38,31 @@ screen.key('a', () => {
   addInput.focus()
 });
 
+const contentForm = blessed.form({
+  parent: screen,
+  left: '51%',
+  height: '97%',
+  border: {
+    type: 'line',
+  },
+});
+
+const contentInput = blessed.textbox({
+  parent: contentForm,
+  inputOnFocus: true,
+});
+
+contentInput.setValue(db.get('notes').value()[0].content);
+
+contentInput.submit(() => {
+  const value = contentInput.getValue();
+  db.set(`notes[${selectedStore.getState().selected}].content`, value).write();
+  screen.render();
+});
+
 module.exports = {
   addInput,
   addForm,
+  contentForm,
+  contentInput,
 };
